815e865e3db0c136dc9591927b6404f3b67de2c1
Wade Fleming <WFleming@caci.co.uk>
2020-04-13 15:58:15 +0100

    show cards on home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,40 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { EventResponse } from '../interfaces';
+import { Subscription } from 'rxjs';
+import { EventsService } from '../events.service';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
+  events: EventResponse[] = [];
+  subscripton: Subscription;
 
-  constructor() {}
+  constructor(
+    private eventsService: EventsService,
+    private navController: NavController
+  ) {}
 
+  ngOnInit() {
+    this.subscripton = this.eventsService
+      .getAll()
+      .subscribe((event) => this.events.push(event));
+  }
+
+  ngOnDestroy() {
+    this.subscripton.unsubscribe();
+  }
+
+  getEvents(): EventResponse[] {
+    return this.events.sort((a, b) =>
+      a.event.created > b.event.created ? -1 : 1
+    );
+  }
+
+  details(response: EventResponse) {
+    this.navController.navigateForward(`/details/${response.event.id}`);
+  }
 }

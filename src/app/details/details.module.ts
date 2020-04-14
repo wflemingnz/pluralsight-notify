001aedc38ffe1842e7ca9e6e2bf42d0f0573bbe5
Wade Fleming <WFleming@caci.co.uk>
2020-04-14 11:33:59 +0100

    add ofline indicator to details page

--- src/app/details/details.module.ts
+++ src/app/details/details.module.ts
@@ -8,9 +8,16 @@ import { DetailsPageRoutingModule } from './details-routing.module';
 
 import { DetailsPage } from './details.page';
 import { DetailsItemComponent } from './details-item/details-item.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  imports: [CommonModule, FormsModule, IonicModule, DetailsPageRoutingModule],
+  imports: [
+    CommonModule,
+    FormsModule,
+    IonicModule,
+    DetailsPageRoutingModule,
+    SharedModule,
+  ],
   declarations: [DetailsPage, DetailsItemComponent],
 })
 export class DetailsPageModule {}

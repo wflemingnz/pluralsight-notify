62bb038603c89c594aa41ec8160cbba2f459def0
Wade Fleming <WFleming@caci.co.uk>
2020-04-14 16:11:17 +0100

    check for updates every hour

--- src/app/shared/updater.service.ts
+++ src/app/shared/updater.service.ts
@@ -1,11 +1,12 @@
-import { OnDestroy, Injectable } from '@angular/core';
-import { Subscription } from 'rxjs';
+import { OnDestroy, Injectable, ApplicationRef } from '@angular/core';
+import { Subscription, interval, concat } from 'rxjs';
 import {
   SwUpdate,
   UpdateAvailableEvent,
   UpdateActivatedEvent,
 } from '@angular/service-worker';
 import { ToastController, AlertController } from '@ionic/angular';
+import { first } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root',
@@ -16,20 +17,46 @@ export class UpdaterService implements OnDestroy {
   constructor(
     private updater: SwUpdate,
     private toastController: ToastController,
-    private alertController: AlertController
+    private alertController: AlertController,
+    private appRef: ApplicationRef
   ) {}
 
   initialise() {
-    this.subscription.add(
-      this.updater.available.subscribe((e) => this.onUpdateAvailable(e))
-    );
+    this.setupUpdateActivated();
+    this.setupImmediateUpdate();
+    this.setupRecurringUpdateCheck();
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  private setupUpdateActivated() {
     this.subscription.add(
       this.updater.activated.subscribe((e) => this.onUpdateActivated(e))
     );
   }
 
-  ngOnDestroy() {
-    this.subscription.unsubscribe();
+  private setupImmediateUpdate() {
+    this.subscription.add(
+      this.updater.available.subscribe((e) => this.onUpdateAvailable(e))
+    );
+  }
+
+  private setupRecurringUpdateCheck() {
+    // Allow the app to stabilize first, before starting polling for updates with `interval()`.
+    const appIsStable$ = this.appRef.isStable.pipe(
+      first((isStable) => isStable === true)
+    );
+    //const everyMinute$ = interval(1 * 60 * 1000);
+    const everyHour$ = interval(1 * 60 * 60 * 1000);
+    const everyIntervalOnceAppIsStable$ = concat(appIsStable$, everyHour$);
+
+    this.subscription.add(
+      everyIntervalOnceAppIsStable$.subscribe(
+        () => this.updater.isEnabled && this.updater.checkForUpdate()
+      )
+    );
   }
 
   private async onUpdateAvailable(e: UpdateAvailableEvent) {
